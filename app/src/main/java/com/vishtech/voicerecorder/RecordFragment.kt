3e4a67c042f2b2824773d0fcb9c1d4be0aa62f62
Vishwajeet Barve <vishtech36@gmail.com>
2021-06-20 13:35:16 +0530

    Audio recording permission added!

--- app/src/main/java/com/vishtech/voicerecorder/RecordFragment.kt
+++ app/src/main/java/com/vishtech/voicerecorder/RecordFragment.kt
@@ -1,15 +1,22 @@
 package com.vishtech.voicerecorder
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.vishtech.voicerecorder.databinding.FragmentRecordBinding
 
 
-class RecordFragment : Fragment(R.layout.fragment_record) {
+class RecordFragment : Fragment(R.layout.fragment_record){
 
     private lateinit var binding : FragmentRecordBinding
+    private var isRecording = false
+    private val RECORD_PERMISSION = Manifest.permission.RECORD_AUDIO
+    private val PERMISSION_CODE = 12
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -19,5 +26,47 @@ class RecordFragment : Fragment(R.layout.fragment_record) {
         binding.recordListBtn.setOnClickListener {
             view.findNavController().navigate(R.id.action_recordFragment_to_audioListFragment)
         }
+
+        binding.recordBtn.setOnClickListener {
+            if(isRecording) {
+                isRecording = false
+                binding.recordBtn.setImageResource(R.drawable.ic_launcher_background)
+            }else{
+                // start recording
+                if(checkPermissions()) {
+                    isRecording = true
+                    binding.recordBtn.setImageResource(R.drawable.ic_mic_24)
+                }
+            }
+        }
+    }
+
+    private fun checkPermissions(): Boolean {
+        return if(ActivityCompat.checkSelfPermission(requireContext(), RECORD_PERMISSION) == PackageManager.PERMISSION_GRANTED) {
+            true
+        } else {
+            ActivityCompat.requestPermissions(requireActivity(),
+                arrayOf(RECORD_PERMISSION),
+                PERMISSION_CODE)
+            false
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
